56069edbe1ff381b0507d0023c3dfe491aa8d421
Hrushikesh2005 <hrushikeshpandit05@student.sfit.ac.in>
2026-01-31 19:10:05 +0530

    new commit

--- customer_churn/customer_churn/settings.py
+++ customer_churn/customer_churn/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='django-insecure-uq*rcist^rhd7*++nz)nz+7cwq$(kve1m(9dr$9l)_5_(+b28y')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
