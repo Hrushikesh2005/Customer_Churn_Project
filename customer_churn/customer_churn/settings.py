ff59dae259ed008a5b0bc1dc70c21f2351fe89a2
Hrushikesh2005 <hrushikeshpandit05@student.sfit.ac.in>
2026-01-31 19:02:06 +0530

    new commit

--- customer_churn/customer_churn/settings.py
+++ customer_churn/customer_churn/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uq*rcist^rhd7*++nz)nz+7cwq$(kve1m(9dr$9l)_5_(+b28y'
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-uq*rcist^rhd7*++nz)nz+7cwq$(kve1m(9dr$9l)_5_(+b28y')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = []
 
